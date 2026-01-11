9c387c5c55a8ce36097319c299a8eed068f765f0
CSutter5 <git@csutter.dev>
2026-01-11 13:32:57 -0600

    - Made Fluctus file loop for testing - Created PTR Tracker telemetry stream

--- ptr-tracker-interface-scripts/ptrTracker.py
+++ ptr-tracker-interface-scripts/ptrTracker.py
@@ -1,21 +1,21 @@
 import paho.mqtt.client as mqtt
 import serial
+import json
 import time
 
 import os
 from dotenv import load_dotenv
 load_dotenv() 
 
-from python.fluctusDecoder import decodeFluctusData
 
-MQTT_TOPIC = "telemetry/fluctus/"
+MQTT_TOPIC = "telemetry/ptr_tracker/"
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0: print("Connected to MQTT broker")
     else:       print(f"Failed to connect, rc={rc}")
 
 if __name__ == "__main__":
-    client = mqtt.Client(client_id="fluctus_telemetry")
+    client = mqtt.Client(client_id="ptr_tracker_telemetry")
     client.on_connect = on_connect
     client.username_pw_set(os.getenv("USERNAME"), os.getenv("PASSWORD"))
     client.connect(os.getenv("MQTT_BROKER"), int(os.getenv("MQTT_PORT")), keepalive=60)
@@ -26,18 +26,15 @@ if __name__ == "__main__":
 
     # Open example file for testing
     while True:
-        file = open("exampleFluctusEncode.txt", "r")
+        file = open("examplePTR.txt", "r")
         # Read a line
         # line = ser.readline().decode('utf-8').strip()
         for line in file:
         # if line:
             # Decode it
-            decoded_data = decodeFluctusData(line)
+            decoded_data = json.loads(line)
             # Send each field as a separate MQTT thread
             for key, value in decoded_data.items():
-                if key == "status" or key == "pyroStates" or key == "message":
-                    value = value["raw"]
-
                 client.publish(f"{MQTT_TOPIC}{key}", value)
                 print(f"{MQTT_TOPIC}{key} -> {value}")
             time.sleep(1)
